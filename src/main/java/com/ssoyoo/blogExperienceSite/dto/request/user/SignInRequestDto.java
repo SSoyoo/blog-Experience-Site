a08a9ef2a4683bc573cce3aba71d775463ab6d23
ssoyoo <soyoo0819@naver.com>
2023-05-04 18:08:14 +0900

    로그인요청dto 작성

---
+++ src/main/java/com/ssoyoo/blogExperienceSite/dto/request/user/SignInRequestDto.java
@@ -0,0 +1,15 @@
+package com.ssoyoo.blogExperienceSite.dto.request.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInRequestDto {
+
+    private String email;
+    private String password;
+    
+}

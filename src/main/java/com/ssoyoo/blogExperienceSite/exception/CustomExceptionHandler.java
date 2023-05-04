18bbb42a31f0f7e11e42659f721660f5da5cb652
ssoyoo <soyoo0819@naver.com>
2023-05-04 15:55:39 +0900

    CustomExceptionHandler 리턴 리팩토링

--- src/main/java/com/ssoyoo/blogExperienceSite/exception/CustomExceptionHandler.java
+++ src/main/java/com/ssoyoo/blogExperienceSite/exception/CustomExceptionHandler.java
@@ -7,24 +7,22 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.ssoyoo.blogExperienceSite.common.util.CustomResponse;
 import com.ssoyoo.blogExperienceSite.dto.response.ResponseDto;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
-    
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ResponseDto> handlerHttpMessageNotReadableException(
             HttpMessageNotReadableException exception) {
-        ResponseDto responseBody = new ResponseDto("VF", "Request Parameter Validation Failed");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+        return CustomResponse.validationFail();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto> handlerMethodArgumentNotValidException(
         MethodArgumentNotValidException exception) {
-        ResponseDto responseBody = new ResponseDto("VF", "Request Parameter Validation Failed");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
-    }
+        return CustomResponse.validationFail();
 
  
 }
